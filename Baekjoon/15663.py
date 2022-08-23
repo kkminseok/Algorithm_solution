efd585584e7f5c3412ddcc6e80b4e4475d60b1f6
kms <aaaa727@khu.ac.kr>
2022-08-23 19:14:41 +0900

    Sliver2 Class4 Problem N과M (9) add

---
+++ Baekjoon/15663.py
@@ -0,0 +1,36 @@
+from sys import stdin
+from copy import deepcopy
+
+
+def recur(listed: list, m: int):
+    pick = []
+    size = len(listed)
+    visit = [False] * size
+    dict = {}
+
+    def combination():
+        pick_data = ' '.join(map(str, pick))
+        if len(pick) == m:
+            dict[pick_data] = 1
+        else:
+            for i in range(size):
+                if visit[i] == True:
+                    continue
+                pick.append(listed[i])
+                visit[i] = True
+                combination()
+                visit[i] = False
+                pick.pop()
+
+    combination()
+    for key, item in dict.items():
+        print(key)
+
+
+def solution():
+    n, m = map(int, stdin.readline().split())
+    input_list = list(map(int, stdin.readline().split()))
+    recur(sorted(input_list), m)
+
+
+solution()
